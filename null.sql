fa0782d0cda3a3d858cd5b41bf2e71867379eeab
shantunu007 <chakmashantunu.web@gmail.com>
2025-12-28 21:32:08 +0600

    Add pagination queries with LIMIT and OFFSET examples

--- null.sql
+++ null.sql
@@ -18,3 +18,44 @@ select
     coalesce(email, 'Not Provided')
 from
     students;
+
+select
+    *
+from
+    students
+limit
+    5;
+
+select
+    *
+from
+    students
+offset
+    5;
+
+select
+    *
+from
+    students
+limit
+    5
+offset
+    0;
+
+select
+    *
+from
+    students
+limit
+    5
+offset
+    5;
+
+select
+    *
+from
+    students
+limit
+    5
+offset
+    10;
