ae1e1caa18147bf4062a908b6c87d6f532f39184
shantunu007 <chakmashantunu.web@gmail.com>
2026-01-05 12:46:32 +0600

    add right join query to fetch users with related posts

--- essentials.sql
+++ essentials.sql
@@ -228,3 +228,23 @@ SELECT
 FROM
     users
     FULL JOIN post ON users.id = post.user_id;
+
+
+--right join
+SELECT
+    users.id,
+    users.name,
+    post.title
+FROM
+    users
+    RIGHT JOIN post ON users.id = post.user_id;
+
+
+--full join
+SELECT
+    users.id,
+    users.name,
+    post.title
+FROM
+    users
+    FULL JOIN post ON users.id = post.user_id;
